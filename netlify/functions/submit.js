fbd4a930fd5ac4bde0837a7fb9398f5b0f9e8ce5
ejoyce-cmd <ejoyce@trianglerenovations.com>
2026-06-10 10:44:57 -0400

    Add files via upload

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -2,15 +2,13 @@ const https = require('https');
 
 const MONDAY_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjY2NzU3OTQwOSwiYWFpIjoxMSwidWlkIjozNzg0OTk1OSwiaWFkIjoiMjAyNi0wNi0wNVQxODoyMDoxOS42ODVaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTM0NzQ4OTYsInJnbiI6InVzZTEifQ.Q6hcQ7t8Jfi8tqgW_RzIlnFb1HbdcbIK1wQV9y0BHLE';
 const MONDAY_BOARD = 18417004194;
+const CC_TOKEN = 'Jc3Lv6lQBoSSCOK1uL17qwDDIAe-lX99LMkW_uwc5cU';
 
 function httpsPost(hostname, path, headers, body) {
   return new Promise((resolve) => {
     const isBuffer = Buffer.isBuffer(body);
     const contentLength = isBuffer ? body.length : Buffer.byteLength(body);
-    const options = {
-      hostname, path, method: 'POST',
-      headers: { ...headers, 'Content-Length': contentLength }
-    };
+    const options = { hostname, path, method: 'POST', headers: { ...headers, 'Content-Length': contentLength } };
     const req = https.request(options, (res) => {
       const chunks = [];
       res.on('data', c => chunks.push(c));
@@ -42,16 +40,10 @@ function generatePDF(address, tech, sections) {
   function esc(s) {
     return String(s || '').replace(/\\/g,'\\\\').replace(/\(/g,'\\(').replace(/\)/g,'\\)').substring(0, 80);
   }
-
   function checkY(needed) {
-    if (yPos < needed) {
-      pageLines.push(currentPage.join('\n'));
-      currentPage = [];
-      yPos = 750;
-    }
+    if (yPos < needed) { pageLines.push(currentPage.join('\n')); currentPage = []; yPos = 750; }
   }
 
-  // Header page content
   currentPage.push('0.106 0.227 0.420 rg');
   currentPage.push('0 750 612 42 re f');
   currentPage.push('1 1 1 rg');
@@ -67,22 +59,18 @@ function generatePDF(address, tech, sections) {
     let allItems = [];
     if (section.type === 'multiroom') {
       (section.rooms || []).forEach(function(room) {
-        (room.items || []).forEach(function(item) {
-          if (item.label) allItems.push({ ...item, roomName: room.name });
-        });
+        (room.items || []).forEach(function(item) { if (item.label) allItems.push({ ...item, roomName: room.name }); });
       });
     } else {
-      (section.items || []).forEach(function(item) {
-        if (item.label) allItems.push({ ...item, roomName: null });
-      });
+      (section.items || []).forEach(function(item) { if (item.label) allItems.push({ ...item, roomName: null }); });
     }
     if (allItems.length === 0) return;
 
     checkY(80);
     currentPage.push('0.106 0.227 0.420 rg');
-    currentPage.push('20 ' + (yPos - 18) + ' 572 20 re f');
+    currentPage.push('20 ' + (yPos-18) + ' 572 20 re f');
     currentPage.push('1 1 1 rg');
-    currentPage.push('BT /F2 10 Tf 28 ' + (yPos - 12) + ' Td (' + esc(section.title) + ') Tj ET');
+    currentPage.push('BT /F2 10 Tf 28 ' + (yPos-12) + ' Td (' + esc(section.title) + ') Tj ET');
     currentPage.push('0 0 0 rg');
     yPos -= 28;
 
@@ -91,116 +79,76 @@ function generatePDF(address, tech, sections) {
       checkY(55);
       const cost = parseFloat(item.cost) || 0;
       if (cost > 0) grandTotal += cost;
-
       if (item.roomName && item.roomName !== currentRoom) {
         currentRoom = item.roomName;
         currentPage.push('0.878 0.906 0.957 rg');
-        currentPage.push('20 ' + (yPos - 16) + ' 572 18 re f');
+        currentPage.push('20 ' + (yPos-16) + ' 572 18 re f');
         currentPage.push('0.106 0.227 0.420 rg');
-        currentPage.push('BT /F2 9 Tf 28 ' + (yPos - 10) + ' Td (' + esc(item.roomName) + ') Tj ET');
+        currentPage.push('BT /F2 9 Tf 28 ' + (yPos-10) + ' Td (' + esc(item.roomName) + ') Tj ET');
         currentPage.push('0 0 0 rg');
-        yPos -= 24;
-        checkY(45);
+        yPos -= 24; checkY(45);
       }
-
       currentPage.push('0.98 0.98 0.98 rg');
-      currentPage.push('20 ' + (yPos - 16) + ' 572 18 re f');
+      currentPage.push('20 ' + (yPos-16) + ' 572 18 re f');
       currentPage.push('0 0 0 rg');
-      currentPage.push('BT /F2 8 Tf 28 ' + (yPos - 10) + ' Td (' + esc(item.label) + ') Tj ET');
-
-      const condColor = item.condition === 'Good' ? '0.15 0.6 0.15 rg' : item.condition === 'Poor' ? '0.753 0.224 0.169 rg' : '0.85 0.6 0.1 rg';
+      currentPage.push('BT /F2 8 Tf 28 ' + (yPos-10) + ' Td (' + esc(item.label) + ') Tj ET');
+      const condColor = item.condition==='Good'?'0.15 0.6 0.15 rg':item.condition==='Poor'?'0.753 0.224 0.169 rg':'0.85 0.6 0.1 rg';
       currentPage.push(condColor);
-      currentPage.push('370 ' + (yPos - 15) + ' 55 14 re f');
+      currentPage.push('370 ' + (yPos-15) + ' 55 14 re f');
       currentPage.push('1 1 1 rg');
-      currentPage.push('BT /F2 7 Tf 373 ' + (yPos - 9) + ' Td (' + esc(item.condition || 'N/A') + ') Tj ET');
-
-      const priColor = item.priority === 'Must Do' ? '0.753 0.224 0.169 rg' : item.priority === 'High' ? '0.85 0.6 0.1 rg' : '0.5 0.5 0.5 rg';
+      currentPage.push('BT /F2 7 Tf 373 ' + (yPos-9) + ' Td (' + esc(item.condition||'N/A') + ') Tj ET');
+      const priColor = item.priority==='Must Do'?'0.753 0.224 0.169 rg':item.priority==='High'?'0.85 0.6 0.1 rg':'0.5 0.5 0.5 rg';
       currentPage.push(priColor);
-      currentPage.push('430 ' + (yPos - 15) + ' 55 14 re f');
+      currentPage.push('430 ' + (yPos-15) + ' 55 14 re f');
       currentPage.push('1 1 1 rg');
-      currentPage.push('BT /F2 7 Tf 433 ' + (yPos - 9) + ' Td (' + esc(item.priority || '-') + ') Tj ET');
-
+      currentPage.push('BT /F2 7 Tf 433 ' + (yPos-9) + ' Td (' + esc(item.priority||'-') + ') Tj ET');
       currentPage.push('0 0 0 rg');
-      if (cost > 0) {
-        currentPage.push('BT /F2 8 Tf 492 ' + (yPos - 10) + ' Td ($' + cost.toFixed(2) + ') Tj ET');
-      }
+      if (cost > 0) currentPage.push('BT /F2 8 Tf 492 ' + (yPos-10) + ' Td ($' + cost.toFixed(2) + ') Tj ET');
       yPos -= 20;
-
-      if (item.comments) {
-        checkY(16);
-        currentPage.push('BT /F1 7 Tf 36 ' + (yPos - 8) + ' Td (Notes: ' + esc(item.comments) + ') Tj ET');
-        yPos -= 13;
-      }
-
-      if (item.photoCount > 0) {
-        checkY(14);
-        currentPage.push('0.1 0.5 0.8 rg');
-        currentPage.push('BT /F1 7 Tf 36 ' + (yPos - 8) + ' Td (' + item.photoCount + ' photo(s) captured) Tj ET');
-        currentPage.push('0 0 0 rg');
-        yPos -= 13;
-      }
+      if (item.comments) { checkY(16); currentPage.push('BT /F1 7 Tf 36 ' + (yPos-8) + ' Td (Notes: ' + esc(item.comments) + ') Tj ET'); yPos -= 13; }
+      if (item.photoCount > 0) { checkY(14); currentPage.push('0.1 0.5 0.8 rg'); currentPage.push('BT /F1 7 Tf 36 ' + (yPos-8) + ' Td (' + item.photoCount + ' photo(s) captured) Tj ET'); currentPage.push('0 0 0 rg'); yPos -= 13; }
     });
     yPos -= 8;
   });
 
-  // Total footer
   checkY(40);
   currentPage.push('0.106 0.227 0.420 rg');
-  currentPage.push('20 ' + (yPos - 30) + ' 572 32 re f');
+  currentPage.push('20 ' + (yPos-30) + ' 572 32 re f');
   currentPage.push('1 1 1 rg');
-  currentPage.push('BT /F2 12 Tf 28 ' + (yPos - 18) + ' Td (TOTAL FIELD COST:) Tj ET');
-  currentPage.push('BT /F2 14 Tf 420 ' + (yPos - 18) + ' Td ($' + grandTotal.toFixed(2) + ') Tj ET');
+  currentPage.push('BT /F2 12 Tf 28 ' + (yPos-18) + ' Td (TOTAL FIELD COST:) Tj ET');
+  currentPage.push('BT /F2 14 Tf 420 ' + (yPos-18) + ' Td ($' + grandTotal.toFixed(2) + ') Tj ET');
   pageLines.push(currentPage.join('\n'));
 
-  // Build PDF binary
-  let pdf = '%PDF-1.4\n';
-  let objNum = 1;
-  const offsets = {};
-
-  const f1Num = objNum++;
-  offsets[f1Num] = pdf.length;
+  let pdf = '%PDF-1.4\n'; let objNum = 1; const offsets = {};
+  const f1Num = objNum++; offsets[f1Num] = pdf.length;
   pdf += f1Num + ' 0 obj\n<</Type /Font /Subtype /Type1 /BaseFont /Helvetica>>\nendobj\n';
-
-  const f2Num = objNum++;
-  offsets[f2Num] = pdf.length;
+  const f2Num = objNum++; offsets[f2Num] = pdf.length;
   pdf += f2Num + ' 0 obj\n<</Type /Font /Subtype /Type1 /BaseFont /Helvetica-Bold>>\nendobj\n';
-
-  const fontDictNum = objNum++;
-  offsets[fontDictNum] = pdf.length;
+  const fontDictNum = objNum++; offsets[fontDictNum] = pdf.length;
   pdf += fontDictNum + ' 0 obj\n<</F1 ' + f1Num + ' 0 R /F2 ' + f2Num + ' 0 R>>\nendobj\n';
-
   const contentNums = [];
   pageLines.forEach(function(pageContent) {
-    const cNum = objNum++;
-    offsets[cNum] = pdf.length;
+    const cNum = objNum++; offsets[cNum] = pdf.length;
     const len = Buffer.byteLength(pageContent);
     pdf += cNum + ' 0 obj\n<</Length ' + len + '>>\nstream\n' + pageContent + '\nendstream\nendobj\n';
     contentNums.push(cNum);
   });
-
   const pagesDictNum = objNum + contentNums.length;
   const pageNums = [];
-  contentNums.forEach(function(cNum, idx) {
-    const pNum = objNum++;
-    pageNums.push(pNum);
-    offsets[pNum] = pdf.length;
+  contentNums.forEach(function(cNum) {
+    const pNum = objNum++; pageNums.push(pNum); offsets[pNum] = pdf.length;
     pdf += pNum + ' 0 obj\n<</Type /Page /Parent ' + pagesDictNum + ' 0 R /MediaBox [0 0 612 792] /Contents ' + cNum + ' 0 R /Resources <</Font ' + fontDictNum + ' 0 R>>>>\nendobj\n';
   });
-
   offsets[pagesDictNum] = pdf.length;
-  pdf += pagesDictNum + ' 0 obj\n<</Type /Pages /Kids [' + pageNums.map(n => n + ' 0 R').join(' ') + '] /Count ' + pageNums.length + '>>\nendobj\n';
+  pdf += pagesDictNum + ' 0 obj\n<</Type /Pages /Kids [' + pageNums.map(n => n+' 0 R').join(' ') + '] /Count ' + pageNums.length + '>>\nendobj\n';
   objNum++;
-
-  const catalogNum = objNum++;
-  offsets[catalogNum] = pdf.length;
+  const catalogNum = objNum++; offsets[catalogNum] = pdf.length;
   pdf += catalogNum + ' 0 obj\n<</Type /Catalog /Pages ' + pagesDictNum + ' 0 R>>\nendobj\n';
-
-  const xrefOffset = pdf.length;
-  const totalObjs = objNum;
+  const xrefOffset = pdf.length; const totalObjs = objNum;
   pdf += 'xref\n0 ' + totalObjs + '\n';
   pdf += '0000000000 65535 f \n';
   for (let i = 1; i < totalObjs; i++) {
-    pdf += (offsets[i] ? String(offsets[i]).padStart(10, '0') : '0000000000') + ' 00000 n \n';
+    pdf += (offsets[i] ? String(offsets[i]).padStart(10,'0') : '0000000000') + ' 00000 n \n';
   }
   pdf += 'trailer\n<</Size ' + totalObjs + ' /Root ' + catalogNum + ' 0 R>>\nstartxref\n' + xrefOffset + '\n%%EOF';
   return Buffer.from(pdf);
@@ -214,11 +162,29 @@ async function uploadPDFToMonday(itemId, pdfBuffer, filename) {
     const filePart = '--' + boundary + '\r\nContent-Disposition: form-data; name="variables[file]"; filename="' + filename + '"\r\nContent-Type: application/pdf\r\n\r\n';
     const footer = '\r\n--' + boundary + '--\r\n';
     const body = Buffer.concat([Buffer.from(headerPart + filePart), pdfBuffer, Buffer.from(footer)]);
-    return httpsPost('api.monday.com', '/v2/file',
-      { 'Authorization': MONDAY_KEY, 'Content-Type': 'multipart/form-data; boundary=' + boundary },
-      body
-    );
+    return httpsPost('api.monday.com', '/v2/file', { 'Authorization': MONDAY_KEY, 'Content-Type': 'multipart/form-data; boundary=' + boundary }, body);
   } catch(e) { return { status: 0, data: e.message }; }
+}
+
+async function createCCProject(address) {
+  const body = JSON.stringify({ project: { name: address, status: 'active' } });
+  const res = await httpsPost('api.companycam.com', '/v2/projects',
+    { 'Authorization': 'Bearer ' + CC_TOKEN, 'Content-Type': 'application/json' }, body);
+  return res.data && res.data.id ? res.data.id : null;
+}
+
+async function uploadPhotoToCC(projectId, photoData, photoName, tag) {
+  try {
+    const base64Data = photoData.split(',')[1];
+    const imgBuffer = Buffer.from(base64Data, 'base64');
+    const boundary = '----CCBound' + Date.now() + Math.random().toString(36).substr(2,5);
+    const part1 = Buffer.from('--' + boundary + '\r\nContent-Disposition: form-data; name="photo[image]"; filename="' + photoName + '"\r\nContent-Type: image/jpeg\r\n\r\n');
+    const part2 = Buffer.from('\r\n--' + boundary + '\r\nContent-Disposition: form-data; name="photo[label]"\r\n\r\n' + tag + '\r\n--' + boundary + '--\r\n');
+    const body = Buffer.concat([part1, imgBuffer, part2]);
+    const res = await httpsPost('api.companycam.com', '/v2/projects/' + projectId + '/photos',
+      { 'Authorization': 'Bearer ' + CC_TOKEN, 'Content-Type': 'multipart/form-data; boundary=' + boundary }, body);
+    return res.status === 200 || res.status === 201;
+  } catch(e) { return false; }
 }
 
 exports.handler = async function(event) {
@@ -227,16 +193,24 @@ exports.handler = async function(event) {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'POST, OPTIONS'
   };
-
   if (event.httpMethod === 'OPTIONS') return { statusCode: 200, headers, body: '' };
   if (event.httpMethod !== 'POST') return { statusCode: 405, headers, body: 'Method not allowed' };
 
   try {
     const payload = JSON.parse(event.body);
-    const { address, tech, reportText, grandTotal, sections } = payload;
-    const results = { monday: false, pdf: false, errors: [] };
+    const { address, tech, reportText, grandTotal, sections, photos, photosOnly, ccProjectId } = payload;
+    const results = { monday: false, pdf: false, companycam: false, photos: 0, errors: [] };
 
-    // Get Monday column IDs
+    // Photos-only mode: just upload more photos to existing CC project
+    if (photosOnly && ccProjectId && photos && photos.length > 0) {
+      for (const photo of photos) {
+        const ok = await uploadPhotoToCC(ccProjectId, photo.data, photo.name || 'photo.jpg', photo.tag);
+        if (ok) results.photos++;
+      }
+      return { statusCode: 200, headers, body: JSON.stringify({ success: true, results }) };
+    }
+
+    // Monday.com
     const boardData = await mondayRequest(`{ boards(ids: [${MONDAY_BOARD}]) { columns { id title type } } }`);
     const cols = (boardData.data && boardData.data.boards && boardData.data.boards[0].columns) || [];
     const colMap = {};
@@ -265,22 +239,33 @@ exports.handler = async function(event) {
 
     if (itemId) {
       results.monday = true;
-      const safeReport = reportText.substring(0, 5000).replace(/\\/g,'\\\\').replace(/"/g,'\\"').replace(/\n/g,'\\n');
+      const safeReport = reportText.substring(0,5000).replace(/\\/g,'\\\\').replace(/"/g,'\\"').replace(/\n/g,'\\n');
       await mondayRequest(`mutation { create_update(item_id: ${itemId}, body: "${safeReport}") { id } }`);
 
       try {
         const pdfBuffer = generatePDF(address, tech, sections);
-        const filename = address.replace(/[^a-zA-Z0-9]/g, '_').substring(0, 40) + '_Scope.pdf';
+        const filename = address.replace(/[^a-zA-Z0-9]/g,'_').substring(0,40) + '_Scope.pdf';
         const pdfRes = await uploadPDFToMonday(itemId, pdfBuffer, filename);
-        if (pdfRes.status === 200 || (pdfRes.data && pdfRes.data.data)) {
-          results.pdf = true;
-        } else {
-          results.errors.push('PDF: ' + JSON.stringify(pdfRes.data).substring(0, 100));
-        }
-      } catch(e) {
-        results.errors.push('PDF error: ' + e.message);
-      }
+        if (pdfRes.status === 200 || (pdfRes.data && pdfRes.data.data)) results.pdf = true;
+        else results.errors.push('PDF: ' + JSON.stringify(pdfRes.data).substring(0,100));
+      } catch(e) { results.errors.push('PDF: ' + e.message); }
     }
+
+    // CompanyCam — create project then upload photos
+    try {
+      const projectId = await createCCProject(address);
+      if (projectId) {
+        results.companycam = true;
+        results.ccProjectId = projectId;
+        if (photos && photos.length > 0) {
+          for (const photo of photos) {
+            const ok = await uploadPhotoToCC(projectId, photo.data, photo.name || 'photo.jpg', photo.tag);
+            if (ok) results.photos++;
+            else results.errors.push('Photo upload failed: ' + photo.name);
+          }
+        }
+      }
+    } catch(e) { results.errors.push('CC: ' + e.message); }
 
     return { statusCode: 200, headers, body: JSON.stringify({ success: true, results }) };
 
